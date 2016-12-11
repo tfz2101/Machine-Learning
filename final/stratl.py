739f336f207352ca84d491da362a30b7f9520fa3
tfz2101 <zhi.tianmu@gmail.com>
2016-12-10 19:31:41 -0500

    Update stratl.py

--- final/stratl.py
+++ final/stratl.py
@@ -45,7 +45,7 @@ class StrategyLearner(object):
     def addEvidence(self, symbol = "IBM", \
         sd=dt.datetime(2008,1,1), \
         ed=dt.datetime(2009,1,1), \
-        sv = 10000): 
+        sv = 10000):
 
         datesIndex = pd.date_range(sd,ed,freq='1D').tolist()
         symbols = [symbol]
@@ -58,10 +58,11 @@ class StrategyLearner(object):
         indicator = pd.concat([MACD,RSI,Boll,Stock_Data], axis = 1, join='inner')
         indicator = indicator.dropna()
         indicator = indicator.reset_index(drop=True)
-        print('Indicator',indicator)
+        #print('Indicator',indicator)
         self.MACD_Bins = [0.5,0.75,1.5,2.25,3.0]
         self.RSI_Bins = [10.0,30.0,50.0,70.0,90.0]
         self. Boll_Bins = [0.5,0.75,1.5,2.25,3.0]
+
         MACD_S = self.getIndicatorStates(indicator.iloc[:,0],self.MACD_Bins)
         RSI_S = self.getIndicatorStates(indicator.iloc[:,1],self.RSI_Bins)
         Boll_S = self.getIndicatorStates(indicator.iloc[:,2],self.Boll_Bins)
@@ -80,36 +81,27 @@ class StrategyLearner(object):
         learner = self.learner
         initial_s = [MACD_S[0],RSI_S[0],Boll_S[0],1]
         initial_s = self.getStateFromDirects(initial_s)
-        print('initial_s',initial_s)
+        #print('initial_s',initial_s)
 
         # add your code to do learning here
         rand.seed(5)
-        for iteration in range(0,1):
+        for iteration in range(0,10):
             action = self.learner.querysetstate(initial_s) #set the state and get first action
             for i in range(1,indicator.shape[0]):
 
                 #move to new location according to action and then get a new action
-                s_prime = self.getStateFromDirects([MACD_S[i],RSI_S[i],Boll_S[i],action])
+                s = action
+                s_prime = self.getStateFromDirects([MACD_S[i],RSI_S[i],Boll_S[i],s])
                 pos = self.getDirectsFromState(learner.s)[3]
                 r = self.computeOneDayPnl(indicator.iloc[:,3],i-1,i,pos)
                 action = learner.query(s_prime,r)
 
-        '''
-        # example use with new colname 
-        volume_all = ut.get_data(syms, dates, colname = "Volume")  # automatically adds SPY
-        volume = volume_all[syms]  # only portfolio symbols
-        volume_SPY = volume_all['SPY']  # only SPY, for comparison later
-        if self.verbose: print volume
-        '''
 
     # this method should use the existing policy and test it against new data
     def testPolicy(self, symbol = "IBM", \
         sd=dt.datetime(2009,1,1), \
         ed=dt.datetime(2010,1,1), \
         sv = 10000):
-
-        # here we build a fake set of trades
-        # your code should return the same sort of data
 
         datesIndex = pd.date_range(sd,ed,freq='1D').tolist()
         symbols = [symbol]
@@ -122,42 +114,34 @@ class StrategyLearner(object):
         RSI = indicators.getRSIValues(Stock_Data,30)
         Boll = indicators.getBollingerValues(Stock_Data,30)
         indicator = pd.concat([MACD,RSI,Boll,Stock_Data], axis = 1, join='inner')
-        indicator = indicator.dropna()
-        indicator = indicator.reset_index(drop=True)
-        print('Indicator',indicator)
-        self.MACD_Bins = [0.5,0.75,1.5,2.25,3.0]
-        self.RSI_Bins = [10.0,30.0,50.0,70.0,90.0]
-        self. Boll_Bins = [0.5,0.75,1.5,2.25,3.0]
+        indicator_raw = indicator.dropna()
+        indicator = indicator_raw.reset_index(drop=True,inplace=False)
+
         MACD_S = self.getIndicatorStates(indicator.iloc[:,0],self.MACD_Bins)
         RSI_S = self.getIndicatorStates(indicator.iloc[:,1],self.RSI_Bins)
         Boll_S = self.getIndicatorStates(indicator.iloc[:,2],self.Boll_Bins)
-        print('MACD_S',MACD_S)
-        print('RSI_S',RSI_S)
-        print('Boll_S',Boll_S)
 
-        num_states = 6553
-        num_actions = 3
-        alpha = 0.2
-        gamma = 0.9
-        rar = 0.5
-        radr = 0.99
-
-        trades =  indicator.iloc[:,3].copy()
+        trades = pd.DataFrame(indicator.iloc[:,3].values,index = indicator_raw.index.values)
         trades.values[:] = 0 # set them all to nothing
-        
-        for i in range(0,trades.shape[0]):
-            
 
-        '''
-        trades.values[8,:] = 1000 # add a BUY at the 9th date
-        if self.verbose: print type(trades) # it better be a DataFrame!
-        if self.verbose: print trades
-        if self.verbose: print prices_all
-        '''
+        curPos = 1
+        for i in range(0,indicator.shape[0]):
+
+            oldPos = curPos
+            #print('oldpos',oldPos)
+            direct = [MACD_S[i],RSI_S[i],Boll_S[i],curPos]
+            state = self.getStateFromDirects(direct)
+            curPos = qTab[state,:].argmax()
+            #print('q row',qTab[state,:])
+            if curPos != oldPos:
+                trade = 500 * (curPos - oldPos)
+                trades.iloc[i,0] = trade
+            #print('curPos',curPos)
+        #print('trades',trades)
         return trades
 
-if __name__=="__main__":
-    print "One does not simply think up a strategy"
+#if __name__=="__main__":
+#    print "One does not simply think up a strategy"
 
 
 strat = StrategyLearner()
