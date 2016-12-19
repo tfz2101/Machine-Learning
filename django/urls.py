b677fb18001895f307b2f6343482d3d00469b0a0
tfz2101 <zhi.tianmu@gmail.com>
2016-12-19 17:47:35 -0500

    Create urls.py

---
+++ django/urls.py
@@ -0,0 +1,25 @@
+"""django_bookmarks URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+#from django.conf.urls.defaults import *
+from bookmarks.views import *
+
+#The r'^ deonotes regular expressions, which interprets all backslashes literally as a string.^ denotes the beginning and $ denotes the end of the literal.
+#The urlpatterns maps python functions to actual pages from the views.py
+urlpatterns = [
+    url(r'^$/', main_page),
+]
