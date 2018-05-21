c49f9a01426a306246e2265c5dc5d4e2bfe4f8b5
tfz2101 <zhi.tianmu@gmail.com>
2018-05-21 14:29:18 -0400

    Update Proj1.py

--- Proj1.py
+++ Proj1.py
@@ -18,19 +18,19 @@ def sense(p, Z, world, pMiss, pHit):
     return q
 
 def move(p, U, pMove, pNoMove):
-    q = np.full((p.shape[0], p.shape[1]), 1.0)
+    q = p.copy()
     direction = ""
-    if abs(U[0]) <= 0.0:
+    if abs(U[0]) > 0.0:
         direction = "V"
         U = U[0]
-    elif abs(U[1]) <= 0.0:
+
+    elif abs(U[1]) > 0.0:
         direction ="H"
         U = U[1]
     if direction == "H":
         for i in range(q.shape[0]):
             for j in range(q.shape[1]):
                 s = pMove * p[i,((j-U) % p.shape[1])]
-                print('1st s',s)
                 s = s + pNoMove * p[i,j]
                 q[i,j] = s
 
@@ -39,13 +39,11 @@ def move(p, U, pMove, pNoMove):
         for i in range(q.shape[0]):
             for j in range(q.shape[1]):
                 s = pMove * p[((i-U) % p.shape[0]),j]
-                print('1st s', s)
                 s = s + pNoMove * p[i,j]
                 q[i,j] = s
     return q
 
-def localize(colors,measurements,motions,sensor_right,p_move):
-        pass
+
 
 
 
@@ -53,26 +51,32 @@ colors = [['G', 'G', 'G'],
           ['G', 'R', 'R'],
           ['G', 'G', 'G']]
 measurements = ['R', 'R']
-motions = [[0,0], [0,0]]
-sensor_right = 0.8
-p_move = 1.0
+motions = [[0,0], [0,1]]
+sensor_right = 1.0
+p_move = 0.5
 
 
-pHit = sensor_right
-pMiss = 1.0 - sensor_right
+def localize(colors, measurements, motions, sensor_right, p_move):
+
+    pHit = sensor_right
+    pMiss = 1.0 - sensor_right
 
 
-world_size = len(colors[0]) * len(colors)
-init_p = 1.0/world_size
+    world_size = len(colors[0]) * len(colors)
+    init_p = 1.0/world_size
 
-p = np.full((len(colors[0]),len(colors)),init_p)
-
-p1 = sense(p,measurements[0],colors, pMiss, pHit)
+    p = np.full((len(colors[0]),len(colors)),init_p)
 
 
-pMove = p_move
-pNoMove = 1- pMove
-for i in range(0,len(motions)):
-    p = sense(p, measurements[i], colors, pMiss=pMiss, pHit=pHit)
-    p = move(p,motions[i],pMove=pMove,pNoMove=pNoMove)
-    print(p)
+    pMove = p_move
+    pNoMove = 1- pMove
+
+    for i in range(0,len(motions)):
+        p = move(p,motions[i],pMove=pMove,pNoMove=pNoMove)
+        p = sense(p, measurements[i], colors, pMiss=pMiss, pHit=pHit)
+
+    return p
+
+p = localize(colors, measurements, motions, sensor_right, p_move)
+
+print(p)
